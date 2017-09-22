23814834d5ef8d12288ceb91cc208d568f3a2e4d
debora <d_kalcheva@abv.bg>
2017-09-22 13:32:31 +0300

    C into Basic Syntax -lab exercises

---
+++ 01. Greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace _01.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!",name);
+        }
+    }
+}
---
+++ 02.Add to numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace _02.Add_to_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var sum = a + b;
+            Console.WriteLine($"{a} + {b} = {sum}");
+        }
+    }
+}
