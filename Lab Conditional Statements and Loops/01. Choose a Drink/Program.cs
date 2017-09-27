504294c1776b23c0212d610e1b7c576a7067f6c1
DesislavaBorisova  Kalcheva- Peychinova <d_kp83@abv.bg>
2017-09-27 23:50:35 +0300

    C# Conditional Statementa and Loops - Exercises
    Homework

---
+++ Lab Conditional Statements and Loops/01. Choose a Drink/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _01.Choose_a_Drink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var professional = Console.ReadLine();
+            if (professional== "Athlete")
+            {
+                Console.WriteLine("Water");
+            }
+            else if ( (professional== "Businessman" ) ||(professional== "Businesswoman"))
+            {
+                Console.WriteLine("Coffee");
+            }
+            else if(professional== "SoftUni Student")
+            {
+                Console.WriteLine("Beer");
+            }
+            else
+            {
+                Console.WriteLine("Tea");
+            }
+            
+        }
+    }
+}
---
+++ Lab Conditional Statements and Loops/05. Word in Plural/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace _05.Word_in_Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string singleWord = Console.ReadLine();
+
+            if(singleWord.EndsWith("y"))
+            {
+                singleWord = singleWord.Remove(singleWord.Length - 1);
+                singleWord += "ies";
+            }
+           
+                
+            else if ((singleWord.EndsWith("o"))|| (singleWord.EndsWith("ch"))|| (singleWord.EndsWith("s"))
+               || (singleWord.EndsWith("sh"))|| (singleWord.EndsWith("x"))|| (singleWord.EndsWith("z")))
+            {
+                singleWord += "es";
+            }
+            else
+            {
+                singleWord += "s";
+            }
+            Console.WriteLine(singleWord);
+        }
+    }
+}
